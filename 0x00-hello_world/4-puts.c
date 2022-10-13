cd8460621162d53e59e8273baeba03641b02f01a
giftdavid101 <giftdavid101@gmail.com>
2022-10-13 12:10:00 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Funtion outputs "Programming is like building a multilingual puzzle"
+ * Return: 0 Funtion has no input
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
