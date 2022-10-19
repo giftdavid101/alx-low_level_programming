457620bc24752e70fe0fc13656facae1daca4353
giftdavid101 <giftdavid101@gmail.com>
2022-10-19 08:58:11 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,39 +8,32 @@
 void print_times_table(int n)
 {
 int x, y, z;
-if (n >= 0 && n <= 14)
+if (n >= 0 && n <= 15)
 {
 for (x = 0; x <= n; x++)
 {
-z = x * y;
-if (z > 99)
+_putchar('0');
+for (y = 1; y <= n; y++)
 {
 _putchar(',');
-_putchar(32);
+_putchar(' ');
+z = x * y;
+if (z <= 99)
+_putchar(' ');
+if (z <= 9)
+_putchar(' ');
+if (z >= 100)
+{
 _putchar((z / 100) + '0');
 _putchar(((z / 10) % 10) + '0');
+}
+else if (z <= 99 && z >= 10)
+{
+_putchar((z / 10) + '0');
+}
 _putchar((z % 10) + '0');
-}
-else if (z > 9)
-{
-_putchar(',');
-_putchar(32);
-_putchar(32);
-_putchar(((z / 10) % 10) + '0');
-_putchar((z % 10) + '0');
-}
-else
-{
-if (y != 0)
-{
-_putchar(',');
-_putchar(32);
-_putchar(32);
-_putchar(32);
-}
-_putchar(z + '0');
-}
 }
 _putchar('\n');
+}
 }
 }
