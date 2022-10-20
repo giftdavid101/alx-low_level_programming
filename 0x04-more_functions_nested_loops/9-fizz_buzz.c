0bb694ede718b14a6a46b46838c526df2929a3b4
giftdavid101 <giftdavid101@gmail.com>
2022-10-20 15:36:00 -0700

    fizz-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,25 +8,19 @@
 int main(void)
 {
 int i = 1;
-while (i <= 100)
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 15 == 0)
-{
+if (i % 15 == 0)
 printf("FizzBuzz");
-}
 else if (i % 3 == 0)
-{
+printf("Fizz");
+else if (i % 5 == 0)
 printf("Buzz");
-}
 else
-{
 printf("%i", i);
-}
-if (i != 100)
-{
-putchar(' ');
-}
-i++;
+if (i < 100)
+printf(" ");
+
 }
 putchar('\n');
 return (0);
