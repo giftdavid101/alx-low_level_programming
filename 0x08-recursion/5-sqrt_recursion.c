1b54ce66aeb2d0be83c73be4da8a09c80bbf7182
giftdavid101 <giftdavid101@gmail.com>
2022-11-02 14:55:31 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,3 +16,19 @@ return (-1);
 }
 return (actual_sqrt_recursion(n, 0));
 }
+
+/**
+ * actual_sqrt_recursion - recurses to find natural sqrt
+ * @n: number to calc sqrt from
+ * @i: iterator
+ * Return: the resulting sqrt
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (actual_sqrt_recursion(n, i + 1));
+}
