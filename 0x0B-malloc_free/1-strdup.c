ad027af8e6489e2356ff073e7485360f88d94fcc
giftdavid101 <giftdavid101@gmail.com>
2022-11-08 20:23:46 -0800

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,19 +10,18 @@
 char *_strdup(char *str)
 {
 char *pstr;
-int i, j = 0;
+int i = 0;
+int j = 0;
 
 pstr = (char *)malloc((sizeof(char) * j) + 1);
 if (pstr == NULL)
 return (NULL);
-for (i = 0; i < j; i++)
-pstr[i] = str[i];
-pstr[j] = '\0';
-return (pstr);
+while (str[j])
+j++;
 
 if (str == NULL)
 return (NULL);
-while (str[j] != '\0')
-j++;
+while ((pstr[i] = str[i]) != '\0')
+i++;
 return (pstr);
 }
