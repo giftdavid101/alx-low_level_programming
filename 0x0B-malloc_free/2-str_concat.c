c1eb904d2507af95460d74a36bf008571ac14668
giftdavid101 <giftdavid101@gmail.com>
2022-11-08 20:04:36 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,29 +10,41 @@
 
 char *str_concat(char *s1, char *s2)
 {
-unsigned int l1, l2, l3, i;
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 char *arr;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+while (s1 && s1[len1])
+len1++;
 
-l1 = 0;
+while (s2 && s2[len2])
+len2++;
 
-while (*(s1 + l1) != '\0')
-{
-l1++;
-}
-l2 = 0;
-
-l3 = l1 + l2;
-arr = (char *) malloc(sizeof(char) * l3 + 1);
+arr = malloc(sizeof(char) * (len1 + len2 + 1));
 if (arr == NULL)
 return (NULL);
-for (i = 0; i < l1; i++)
+
+i = 0;
+j = 0;
+
+if (s1)
+{
+while (i < len1)
+{
 arr[i] = s1[i];
-for (i = 0; i < l2; i++)
-arr[i + l1] = s2[i];
+i++;
+}
+}
+
+if (s2)
+{
+while (i < (len1 + len2))
+{
+arr[i] = s2[j];
+i++;
+j++;
+
+}
+}
+arr[i] = '\0';
 return (arr);
 }
