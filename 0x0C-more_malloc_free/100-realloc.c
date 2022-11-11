be6a4d7704499d74d875b649f1f0eb361ba7869c
giftdavid101 <giftdavid101@gmail.com>
2022-11-10 20:03:26 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,14 +14,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 char *ptr1, *old_ptr;
 unsigned int i;
 
-
-if (new_size == 0 && ptr)
-{
-free(ptr);
-return (NULL);
-}
 if (new_size == old_size)
 return (ptr);
+
+if (new_size == 0 && ptr)
+free(ptr);
+return (NULL);
 if (!ptr)
 return (malloc(new_size));
 ptr1 = malloc(new_size);
