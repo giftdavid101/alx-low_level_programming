54bf31d0ce3bb8ff2c5190843ce6bf5747e60db7
giftdavid101 <giftdavid101@gmail.com>
2022-11-17 17:29:22 -0800

    1-print_numbers.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef VARIADIC_FUNCS
 #define VARIADIC_FUNCS
-#define FORMAT(f) ("%"#f)
+
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * struct print_form - Define a struct to figure out how to print
@@ -8,13 +10,8 @@
  * @f: Pointer to function to call
  */
 
-
-typedef struct print_form
-{
-char *c;
-void (*f)();
-}print_form_t;
-
 int sum_them_all(const unsigned int n, ...);
-
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
