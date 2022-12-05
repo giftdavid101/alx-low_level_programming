aadfecb29c8c4c37c6971a09a33a101b7efee45a
giftdavid101 <giftdavid101@gmail.com>
2022-12-05 09:09:22 -0800

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <stddef.h>
 #include <string.h>
 
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-size_t read_textfile(const char *filename, size_t letters);
+
 #endif
