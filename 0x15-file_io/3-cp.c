cbf2e420284240263d0fc7a468e87a76cf652e1f
giftdavid101 <giftdavid101@gmail.com>
2022-12-05 17:12:14 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,9 +10,9 @@
 
 int main(int argc, char **argv)
 {
-int fd;
+int f, d;
 char *buf[BUFSIZ] = { 0 };
-char *file_to, *file_from;
+char *to, *from;
 
 if (argc != 3)
 {
@@ -20,25 +20,27 @@ dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
 exit(97);
 }
 
-file_to = strdup(argv[1]);
-file_from = strdup(argv[2]);
-fd = open(file_to, O_WRONLY | O_TRUNC, 0600);
-if (fd < 1)
+to = strdup(argv[1]);
+from = strdup(argv[2]);
+f = open(from, O_RDONLY, 0600);
+if (f < 1)
 {
-return (-1);
-}
-fd = open(file_from, O_RDONLY | O_CREAT, 0664);
-if (fd < 1)
-{
-printf("Error: Can't read from file %s", file_from);
+printf("Error: Can't read from file %s", from);
 exit(-1);
 }
-close(fd);
-if (fd < 0)
+d = open(to, O_CREAT, O_WRONLY | O_TRUNC, 0664);
+if (d < 1)
 {
-printf("Error: Can't close fd %d", fd);
-exit(100);
+printf("Error: Can't write to %s", to);
+exit(99);
 }
+
+close(f);
+confirm_close_fd(f);
+close(d);
+confirm_close_fd(d);
+free(to);
+free(from);
 return (0);
 }
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <stddef.h>
 #include <string.h>
 
+int confirm_close_fd(int fd);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
