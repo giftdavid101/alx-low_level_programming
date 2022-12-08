9647ccf1acf0fae6b985f33e41899fc67a82e342
giftdavid101 <giftdavid101@gmail.com>
2022-12-07 17:52:33 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,37 +10,114 @@
 
 int main(int argc, char **argv)
 {
-int f, d;
-char *buf[BUFSIZ] = { 0 };
-char *to, *from;
+int f, d, ret = 1;
+char *to, *from, *buf, *tmp;
 
-if (argc != 3)
-{
-dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-exit(97);
-}
+check_argc(argc);
+from = strdup(argv[1]);
+to = strdup(argv[2]);
 
-to = strdup(argv[1]);
-from = strdup(argv[2]);
-f = open(from, O_RDONLY, 0600);
-if (f < 1)
-{
-printf("Error: Can't read from file %s", from);
-exit(-1);
-}
-d = open(to, O_CREAT, O_WRONLY | O_TRUNC, 0664);
-if (d < 1)
-{
-printf("Error: Can't write to %s", to);
-exit(99);
-}
+f = open_file(from, O_RDONLY);
+d = open_file(to, O_CREAT | O_RDWR | 0664);
 
-close(f);
+buf = calloc(BFSZ, sizeof(char));
+if (!buf)
+{
 confirm_close_fd(f);
-close(d);
 confirm_close_fd(d);
-free(to);
+return (-1);
+}
+
+ret = read_file(f, buf);
+while (ret >= 1)
+{
+write(d, buf, ret);
+tmp = realloc(buf, BFSZ + 1);
+if (!tmp)
+{
+free(buf);
+confirm_close_fd(f);
+confirm_close_fd(d);
+return (-1);
+}
+
+buf = tmp;
+ret = read(f, buf, sizeof(buf));
+}
 free(from);
+free(to);
+confirm_close_fd(f);
+confirm_close_fd(d);
 return (0);
 }
 
+/**
+ * open_file - opens and then checks if success
+ * @file: the file descriptor
+ * @oflags: the name of the file
+ * Return: file descriptor
+ */
+
+int open_file(char *file, int oflags)
+{
+int fd = open(file, oflags, 0664);
+if (fd < 1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+exit(-1);
+}
+
+return (fd);
+}
+
+/**
+ * check_argc - checks the number of arguments passed
+ * @argc: the number of arguments
+ */
+
+void check_argc(int argc)
+{
+if (argc != 3)
+{
+dprintf(2, "Usage: cp file_from file_to\n");
+exit(97);
+}
+}
+
+/**
+ * confirm_close_fd - confirm that a file descriptor closes succesfully
+ * @fd: file descriptor
+ * Return: 0 on success
+ */
+
+int confirm_close_fd(int fd)
+{
+close(fd);
+if (fcntl(fd, F_GETFD) == -1)
+{
+return (0);
+}
+else
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+}
+/**
+ * read_file - read a file, and handle failure
+ *  @fd: file descriptor
+ *  @file: the name of the file that was read from
+ *  Return: return value from read operation
+ */
+
+int read_file(int fd, char *file)
+{
+int ret;
+ret = read(fd, file, sizeof(file));
+if (ret < 1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+exit(-1);
+}
+return (ret);
+}
